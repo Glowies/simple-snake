85c8a1b0fcb6f80c61405cc457fb2c5643686961
oktaycomu@gmail.com <>
2022-12-26 16:22:13 -0500

    feat: body movement

---
+++ Assets/Code/Snake/Snake.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public int Length = 1;
+    public float Speed = 4f;
+    public Vector3 Direction = Vector3.right;
+    public bool IsMoving = true;
+    public SnakeBodyMover SnakeBodyMover;
+
+    private IEnumerator _moveRoutine;
+
+    private void Start()
+    {
+        StartMoving();
+    }
+
+    public void TurnLeft()
+    {
+        Direction = Quaternion.AngleAxis(90, Vector3.up) * Direction;
+    }
+
+    public void TurnRight()
+    {
+        Direction = Quaternion.AngleAxis(-90, Vector3.up) * Direction;
+    }
+
+    public void StartMoving()
+    {
+        if(_moveRoutine != null)
+        {
+            return;
+        }
+
+        IsMoving = true;
+        _moveRoutine = MoveSnake();
+        StartCoroutine(_moveRoutine);
+    }
+
+    public void StopMoving()
+    {
+        if(_moveRoutine == null)
+        {
+            return;
+        }
+
+        IsMoving = false;
+        StopCoroutine(_moveRoutine);
+        _moveRoutine = null;
+    }
+
+    IEnumerator MoveSnake()
+    {
+        while(IsMoving)
+        {
+            transform.localPosition += Vector3.forward;
+            SnakeBodyMover.UpdateBodyTransforms(this);
+            yield return new WaitForSeconds(1f/Speed);
+        }
+    }
+}
---
+++ Assets/Code/Snake/SnakeBodyMover.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+record TransformCopyValues
+{
+    public Vector3 localPosition;
+    public Vector3 localScale;
+    public Quaternion localRotation;
+}
+
+public class SnakeBodyMover : MonoBehaviour
+{
+    public GameObject BodyPrefab;
+
+    private Queue<TransformCopyValues> _bodyTransforms;
+    private List<GameObject> _bodyParts;
+
+    void Awake()
+    {
+        _bodyTransforms = new();
+        _bodyParts = new();
+    }
+
+    public void UpdateBodyTransforms(Snake snake)
+    {
+        AddRemoveBodyParts(snake);
+
+        // Add head transform to queue
+        var transformCopy = GetCopyValues(snake.transform);
+        _bodyTransforms.Enqueue(transformCopy);
+        while(_bodyTransforms.Count > snake.Length)
+        {
+            _bodyTransforms.Dequeue();
+        }
+
+        // Get transforms array
+        var bodyTransforms = _bodyTransforms.ToArray();
+
+        for(int i=0; i<_bodyParts.Count; i++)
+        {
+            var bodyPartTransform = _bodyParts[i].transform;
+            var refIndex = Mathf.Min(i + 1, _bodyTransforms.Count - 1);
+            var refTransform = bodyTransforms[refIndex];
+            CopyTransform(in refTransform, ref bodyPartTransform);
+        }
+    }
+
+    TransformCopyValues GetCopyValues(Transform refTransform)
+    {
+        return new TransformCopyValues
+        {
+            localPosition = refTransform.localPosition,
+            localScale = refTransform.localScale,
+            localRotation = refTransform.localRotation,
+        };
+    }
+
+    void CopyTransform(
+        in TransformCopyValues refTransform,
+        ref Transform targetTransform)
+    {
+        targetTransform.localPosition = refTransform.localPosition;
+        targetTransform.localScale = refTransform.localScale;
+        targetTransform.localRotation = refTransform.localRotation;
+    }
+
+    void AddRemoveBodyParts(Snake snake)
+    {
+        var targetLength = snake.Length - 1;
+        var diff = _bodyParts.Count - targetLength;
+        if (diff >= 0)
+        {
+            // Destroy excess body parts
+            for (int i = 0; i < diff; i++)
+            {
+                var toRemove = _bodyParts[targetLength + i];
+                DestroyImmediate(toRemove);
+            }
+
+            _bodyParts.RemoveRange(targetLength, diff);
+            return;
+        }
+
+        // Add new body parts
+        diff *= -1;
+        for(int i=0; i<diff; i++)
+        {
+            var newBody = Instantiate(BodyPrefab, snake.transform.parent);
+            newBody.name = $"Body{_bodyParts.Count}";
+            _bodyParts.Add(newBody);
+        }
+    }
+}
